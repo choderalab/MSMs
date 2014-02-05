809d9586800897d79f53c3daa3d518b293752b0f
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-02-05 13:48:50 -0500

    Added goodtrim

---
+++ src/code/goodtrim.py
@@ -0,0 +1,17 @@
+import mdtraj.io
+import mdtraj as md
+import numpy as np
+
+ass = md.io.loadh("./Data/Assignments.Fixed.h5")["arr_0"]
+
+k = 0
+for i in range(500):
+    print(i)
+    ind = np.where(ass[i] != -1)[0]
+    if len(ind) <= 1:
+        continue
+    start = ind[0]
+    end = ind[-1]
+    trj = md.load("./Trajectories/trj%d.h5" % i)[start:end]
+    trj.save("./goodtrimmed/Trajectories/trj%d.h5" % k)
+    k += 1
